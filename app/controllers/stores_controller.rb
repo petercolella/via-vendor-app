43f49b6ffe5375052face3cd0d55bf960c54557d
petercolella <peter.joseph.colella@gmail.com>
2017-05-11 13:55:22 -0400

    finished crud for stores

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -3,8 +3,61 @@ class StoresController < ApplicationController
     @stores = Store.all
   end
 
-  def new
-  	@store = Store.new
-	@store.build_address
+  def show
+    @store = Store.find(params[:id])
   end
+
+  def new
+  	@company = Company.find(params[:company_id])
+  	@store = Store.new
+    @store.build_address
+  end
+
+  def edit
+    @store = Store.find(params[:id])
+  end
+
+  def create
+    @store = Store.new(store_params)
+    @company = @store.company
+
+    respond_to do |format|
+      if @store.save
+        format.html { redirect_to @store, notice: 'Store was successfully created.' }
+        format.json { render :show, status: :created, location: @store }
+      else
+        format.html { render :new }
+        format.json { render json: @store.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @store.update(store_params)
+        format.html { redirect_to @store, notice: 'Store was successfully updated.' }
+        format.json { render :show, status: :ok, location: @store }
+      else
+        format.html { render :edit }
+        format.json { render json: @store.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @store.destroy
+    respond_to do |format|
+      format.html { redirect_to stores_url, notice: 'Store was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_store
+      @store = Store.find(params[:id])
+    end
+
+    def store_params
+      params.require(:store).permit(:store_name, :store_phone, :company_id)
+    end
 end
