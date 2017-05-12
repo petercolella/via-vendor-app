86f30a3b63fc86a5b82d77197d95b43aed7d250c
petercolella <peter.joseph.colella@gmail.com>
2017-05-12 13:32:17 -0400

    changed app controller due to registration problem in heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # protected
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :phone])
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :phone])
+  end
 end
