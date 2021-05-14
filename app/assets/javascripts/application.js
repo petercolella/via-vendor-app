b99345845491582b21b95d9ee743ae3a77385c8f
petercolella <peter.joseph.colella@gmail.com>
2021-05-13 23:20:44 -0400

    updates application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,11 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require materialize
-//= require angular/angular
-//= require angular-animate/angular-animate
-//= require angular-aria/angular-aria
-//= require angular-material/angular-material
-//= require angular-ui-router/release/angular-ui-router
 //= require_self
 //= require_tree .
 
